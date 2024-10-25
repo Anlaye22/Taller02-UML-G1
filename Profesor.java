7b74105237952c604a44e26a879b53e2c9680176
Xavih830 <120606644+Xavih830@users.noreply.github.com>
2024-10-24 21:48:28 -0500

    Create Profesor.java

---
+++ Profesor.java
@@ -0,0 +1,16 @@
+public class Profesor extends PersonalAcadémico {
+
+  private ArrayList<Curso> cursos;
+  
+  public void cargarActividadSumativa (Curso curso, ActividadSumativa actividadSumativa) {
+  }
+
+  public void calificarActividadSumativa (Curso curso, ActividadSumativa actividadSumativa, Estudiante estudiante) {
+  }
+
+  public void aceptarInscripcion (Curso curso, String usuarioEstudiante) {
+  }
+
+  public void crearNuevoForo (Foro foro) {
+  }
+}
