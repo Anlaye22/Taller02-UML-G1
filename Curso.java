ff9950eda0deff261c6e893fb3e5396e4c7639e4
Xavih830 <120606644+Xavih830@users.noreply.github.com>
2024-10-24 21:59:34 -0500

    Update Curso.java

---
+++ Curso.java
@@ -0,0 +1,20 @@
+public class Curso {
+  private String id;
+  private String nombre;
+  private boolean estadoDisponible;
+  private ArrayList<Estudiante> estudiantesEnEsperaDeInscripcion;
+  private ArrayList<Estudiante> estudiantesInscritos;
+  private Profesor responsable;
+
+  public Curso() {
+  }
+
+  public void crear(String i, String n, boolean e, Arraylist<Estudiante> eeeI, ArrayList<Estudiante> eI, Profesor r) {
+    id = i;
+    nombre = n;
+    estadoDisponible = e;
+    estudiantesEnEsperaDeInscripcion = eeeI;
+    estudiantesInscritos = eI;
+    responsable = r;
+  }
+}
