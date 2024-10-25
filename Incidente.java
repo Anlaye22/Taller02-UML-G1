267183df9da739e933f64319ff0e24293aee6a00
Xavih830 <120606644+Xavih830@users.noreply.github.com>
2024-10-24 21:29:54 -0500

    Update Incidente.java

---
+++ Incidente.java
@@ -0,0 +1,11 @@
+public class Incidente {
+  protected int id;
+  protected Datetime fecha_reportado;
+  protected String estado;
+  protected String titulo;
+  protected String description;
+  protected List<Object> log_actualizaciones;
+  protected Datetime fecha_cerrado;
+  protected String tipo;
+  protected Usuario responsable;
+}
